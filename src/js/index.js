35c249d108bf8aba151b48e62948e2e937ebb58d
Dawid Majewski <dawidmajewski1991@gmail.com>
2022-09-16 09:43:01 +0200

    whole changes - repos, github api fetch, hydrapp, pwa

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,105 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+let isDark = false;
+
+const button = document.querySelector('.mode--js');
+
+button.addEventListener('click', () => {
+    if (isDark == false) {
+        document.documentElement.style.setProperty('--background-color', 'black');
+        document.documentElement.style.setProperty('--block-color', 'rgb(11, 12, 46)');
+        document.documentElement.style.setProperty('--text-color', 'white');
+        isDark = true;
+        button.innerHTML="lightmode";
+    } else {
+        document.documentElement.style.setProperty('--background-color', 'rgb(255, 255, 255)');
+        document.documentElement.style.setProperty('--block-color', 'rgb(212, 212, 212)');
+        document.documentElement.style.setProperty('--text-color', 'black');
+        isDark = false;
+        button.innerHTML="darkmode";
+    };
+});
+
+/*-----------------------------------------------------*/
+const entry = localStorage.getItem('entry');
+
+let result = '';
+
+if (entry) {
+    result = entry;
+}
+
+const textData = document.querySelector('.form__textarea--js');
+textData.value = result;
+
+const loadData = document.querySelector('.load--js');
+const saveData = document.querySelector('.save--js');
+const clearData = document.querySelector('.clear--js');
+
+
+saveData.addEventListener('click', () => {
+    localStorage.setItem('entry', textData.value);
+});
+
+loadData.addEventListener('click', () => {
+    const loadedData = localStorage.getItem('entry');
+    textData.value = loadedData;
+    console.log(textData.innerHTML);
+});
+
+clearData.addEventListener('click', () => {
+    localStorage.removeItem('entry');
+    textData.value = '';
+});
+
+
+fetch("https://api.github.com/users/dawidmajewski1991/repos")
+.then(resp => resp.json())
+.then(resp => {
+    for (let repo of resp) {
+        const {name, html_url} = repo;
+        const repositoryList = document.querySelector('.repobox__placeholder--js');
+        const repoTemplate = `<li class="repobox__item">${name}<a href="${html_url}" class="repobox__link"> [link]</a></li>`;
+
+        repositoryList.innerHTML += repoTemplate;
+    }
+})
+.catch(error => {
+    console.log('brak odpowiedzi.')
+})
+
+let glassCounter = document.querySelector('.hydrapp__digit--js');
+const glassAdd = document.querySelector('.hydrapp__button--js');
+const glassRemove = document.querySelector('.hydrapp__remove--js');
+glassCounter.innerHTML = 0;
+
+
+const key = new Date(). toISOString().slice(0, 10);
+console.log(key);
+ 
+if (localStorage.getItem(key)) {
+} else {
+    localStorage.setItem(key, '0');
+}
+
+glassAdd.addEventListener('click', () => {
+    glassCounter.innerHTML ++;
+    localStorage.setItem(key, glassCounter.innerHTML);
+    console.log('Glass added.');
+});
+
+glassRemove.addEventListener('click', () => {
+    if (glassCounter.innerHTML > 0) {
+    glassCounter.innerHTML --;
+    localStorage.setItem(key, glassCounter.innerHTML);
+    console.log('Glass removed.');
+    } else {
+        console.log(`Can't be less than 0.`);
+    };
+});
--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,6 @@ module.exports = {
   /* here you can define another js file */
   entry: {
     index: "./src/js/index.js",
-    another: "./src/js/another.js",
   },
   output: {
     filename: "[name].[hash:8].js",
@@ -85,7 +84,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/pages/another.html",
       inject: true,
-      chunks: ["index", "another"],
+      chunks: ["index"],
       filename: "another.html",
     }),
   ],
